baaccfde6bd38a0a93c60a2f3ff2010c4bef6525
BlackPhlox <theluja@gmail.com>
2021-08-05 15:02:54 +0200

    Moved to example and updated readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # bevy_dolly
 
 [Dolly](https://github.com/h3r2tic/dolly), now in bevy.
+
+## How to run
+
+`cargo run --release --example orbit`
--- examples/orbit.rs
+++ examples/orbit.rs
@@ -11,7 +11,6 @@ struct MainCamera;
 fn main() {
     App::build()
         .insert_resource(Msaa { samples: 4 })
-        //.insert_resource(Dolly { rigs: vec![] })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .add_system(update_camera.system())
