d54bc8adc0fc82e2d438cd832986000b589b2837
BlackPhlox <theluja@gmail.com>
2021-08-05 03:12:49 +0200

    ...

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,16 @@ use bevy::prelude::*;
 use dolly::glam::Vec3;
 use dolly::prelude::{Arm, CameraRig, Smooth, YawPitch};
 
-struct Dolly{
-    camera: CameraRig
+struct Dolly {
+    rigs: Vec<CameraRig>,
 }
-
-
-impl Default for Dolly {
-    fn default() -> Self {
-        Dolly{camera: CameraRig{drivers: vec![], transform: dolly::transform::Transform::IDENTITY }}
-    }
-}
-
 
 struct MainCamera;
 
 fn main() {
     App::build()
         .insert_resource(Msaa { samples: 4 })
+        .insert_resource(Dolly { rigs: vec![] })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .add_system(update_camera.system())
@@ -29,7 +22,7 @@ fn main() {
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut rig : ResMut<Dolly>,
+    mut rig: ResMut<Dolly>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     // plane
@@ -45,23 +38,25 @@ fn setup(
             material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
             transform: Transform::from_xyz(0.0, 0.5, 0.0),
             ..Default::default()
-        }).id();
-    
+        })
+        .id();
+
     let camera = CameraRig::builder()
         .with(YawPitch::new().yaw_degrees(45.0).pitch_degrees(-30.0))
         .with(Smooth::new_look(1.5))
         .with(Arm::new(Vec3::Z * 4.0))
         .build();
 
-    commands.spawn().insert(Dolly{camera});
+    commands.spawn().insert(Dolly { rigs: vec![camera] });
 
     commands
         .spawn_bundle(PerspectiveCameraBundle {
-            transform: Transform::from_xyz(-2.0, 10.0, 5.0).looking_at(bevy::math::Vec3::ZERO, bevy::math::Vec3::Y),
+            transform: Transform::from_xyz(-2.0, 10.0, 5.0)
+                .looking_at(bevy::math::Vec3::ZERO, bevy::math::Vec3::Y),
             ..Default::default()
         })
         .insert(MainCamera);
-        
+
     // light
     commands.spawn_bundle(LightBundle {
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
@@ -71,12 +66,15 @@ fn setup(
 
 fn update_camera(
     keys: Res<Input<KeyCode>>,
-    mut rig_opt : ResMut<Option<Dolly>>,
+    mut dolly: ResMut<Dolly>,
     mut query: Query<(&mut Transform, With<MainCamera>)>,
 ) {
-    if rig_opt.is_none() {return};
+    if dolly.rigs.len() <= 0 {
+        return;
+    };
+    let rig = dolly.rigs.last().unwrap();
     let (mut cam, _) = query.single_mut().unwrap();
-    let camera_driver = rig_opt.unwrap().camera.driver_mut::<YawPitch>();
+    let camera_driver = rig.driver_mut::<YawPitch>();
     if keys.just_pressed(KeyCode::Z) {
         camera_driver.rotate_yaw_pitch(-90.0, 0.0);
     }
@@ -84,7 +82,7 @@ fn update_camera(
         camera_driver.rotate_yaw_pitch(90.0, 0.0);
     }
 
-    let transform = rig_opt.unwrap().camera.update(0.1);
+    let transform = rig.update(0.1);
     let translation = transform.translation;
     let rotation = transform.rotation;
 
