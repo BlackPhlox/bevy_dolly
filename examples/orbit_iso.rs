e4be77d18260fa548cbf4a2cd128ed567c5b0646
BlackPhlox <theluja@gmail.com>
2021-08-25 22:36:53 +0200

    Added isometric example

---
+++ examples/orbit_iso.rs
@@ -0,0 +1,89 @@
+use bevy::prelude::*;
+use bevy_dolly::Transform2Bevy;
+use dolly::glam::Vec3;
+use dolly::prelude::{Arm, CameraRig, Smooth, YawPitch};
+
+struct MainCamera;
+
+fn main() {
+    App::build()
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .add_system(update_camera.system())
+        .run();
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+
+    commands
+        .spawn_bundle((
+            Transform {
+                translation: bevy::math::Vec3::new(0., 0.2, 0.),
+                ..Default::default()
+            },
+            GlobalTransform::identity(),
+        ))
+        .with_children(|cell| {
+            cell.spawn_scene(asset_server.load("poly_dolly.gltf#Scene0"));
+        })
+        .id();
+
+    commands.spawn().insert(
+        CameraRig::builder()
+            .with(YawPitch::new().yaw_degrees(45.0).pitch_degrees(-30.0))
+            .with(Smooth::new_rotation(1.5))
+            .with(Arm::new(Vec3::Z * 4.0))
+            .build(),
+    );
+
+    let mut camera = OrthographicCameraBundle::new_3d();
+    camera.orthographic_projection.scale = 3.0;
+    camera.transform = Transform::from_xyz(5.0, 5.0, 5.0).looking_at(bevy::math::Vec3::ZERO, bevy::math::Vec3::Y);
+
+    commands
+        .spawn_bundle(camera)
+        .insert(MainCamera);
+
+    // light
+    commands.spawn_bundle(LightBundle {
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
+}
+
+fn update_camera(
+    keys: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut query: QuerySet<(
+        Query<(&mut Transform, With<MainCamera>)>,
+        Query<&mut CameraRig>,
+    )>,
+) {
+    let mut rig = query.q1_mut().single_mut().unwrap();
+    let camera_driver = rig.driver_mut::<YawPitch>();
+
+    if keys.just_pressed(KeyCode::Z) {
+        camera_driver.rotate_yaw_pitch(-90.0, 0.0);
+    }
+    if keys.just_pressed(KeyCode::X) {
+        camera_driver.rotate_yaw_pitch(90.0, 0.0);
+    }
+
+    let transform = rig.update(time.delta_seconds());
+    let (mut cam, _) = query.q0_mut().single_mut().unwrap();
+
+    cam.transform_2_bevy(transform);
+}
