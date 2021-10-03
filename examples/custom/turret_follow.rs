93429d611bb495df60b7d74b59107f989df0ffd6
BlackPhlox <theluja@gmail.com>
2021-10-03 14:01:31 +0200

    Added tracking turret example using rotation lock driver

--- examples/custom/turret_follow.rs
+++ examples/custom/turret_follow.rs
@@ -1,8 +1,8 @@
-use std::f32::consts::FRAC_PI_8;
+use std::f32::consts::{FRAC_2_PI, FRAC_PI_2, FRAC_PI_4, FRAC_PI_8, PI};
 
 use bevy::prelude::*;
 use bevy_dolly::{DollyPlugins, Transform2Bevy, Transform2Dolly, UpdateYawPitch, ZeroedYRotation};
-use dolly::prelude::{Arm, CameraRig, Rotation, Smooth, YawPitch};
+use dolly::prelude::{Arm, Axis, CameraRig, LockRotation, LookAt, Rotation, Smooth, YawPitch};
 
 struct MainCamera;
 
@@ -18,6 +18,7 @@ fn main() {
         .add_startup_system(setup.system())
         .add_system(update_turret.system())
         .add_system(update_barrels.system())
+        .add_system(rotator_system.system())
         .run();
 }
 
@@ -37,11 +38,24 @@ fn setup(
 
     commands
         .spawn_bundle(PerspectiveCameraBundle {
-            transform: Transform::from_xyz(0.0, 2.0, 5.0)
+            transform: Transform::from_xyz(0.0, 3.5, 8.0)
                 .looking_at(bevy::math::Vec3::ZERO, bevy::math::Vec3::Y),
             ..Default::default()
         })
         .insert(MainCamera);
+
+    let poly_dolly_transform = Transform {
+        translation: Vec3::new(4., 2., 0.),
+        rotation: bevy::math::Quat::from_rotation_y(PI),
+        ..Default::default()
+    };
+
+    commands
+        .spawn_bundle((poly_dolly_transform, GlobalTransform::identity()))
+        .with_children(|cell| {
+            cell.spawn_scene(asset_server.load("poly_dolly.gltf#Scene0"));
+        })
+        .insert(Rotates);
 
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Box::new(1., 0.5, 1.))),
@@ -57,6 +71,14 @@ fn setup(
                 .with(Rotation::new(dolly::glam::Quat::IDENTITY))
                 .with(Smooth::new_rotation(1.5))
                 .with(Arm::new(dolly::glam::Vec3::new(0., 0.5, 0.)))
+                .with(
+                    LookAt::new(
+                        poly_dolly_transform.transform_2_dolly().position + dolly::glam::Vec3::Y,
+                    )
+                    .tracking_smoothness(1.25)
+                    .tracking_predictive(true),
+                )
+                .with(LockRotation::new(&[Axis::X, Axis::Z]))
                 .build(),
         )
         .insert(TurretRig);
@@ -106,36 +128,24 @@ fn setup(
 }
 
 fn update_turret(
-    keys: Res<Input<KeyCode>>,
     time: Res<Time>,
     mut query: QuerySet<(
         Query<(&mut Transform, With<Turret>)>,
         Query<(&mut CameraRig, With<TurretRig>)>,
+        Query<(&mut Transform, With<Rotates>)>,
     )>,
 ) {
+    let poly_dolly = query.q2_mut().single_mut().unwrap();
+    let target = poly_dolly.0.transform_2_dolly().position + dolly::glam::Vec3::Y;
+
     let mut rig = query.q1_mut().single_mut().unwrap();
-    let camera_driver = rig.0.driver_mut::<Rotation>();
-
-    let (_, mut rotation) = camera_driver.rotation.to_axis_angle();
-    if keys.pressed(KeyCode::D) {
-        if rotation > std::f32::consts::FRAC_PI_2 * 4.0 - 0.05 {
-            rotation = 0.0;
-        }
-        rotation += 0.05
-    }
-    if keys.pressed(KeyCode::A) {
-        if rotation < 0.05 {
-            rotation = std::f32::consts::FRAC_PI_2 * 4.0;
-        }
-        rotation -= 0.05
-    }
-
-    camera_driver.rotation = dolly::glam::Quat::from_rotation_y(rotation);
+    let camera_driver = rig.0.driver_mut::<LookAt>();
+    camera_driver.target = target;
 
     let transform = rig.0.update(time.delta_seconds());
-    let (mut cam, _) = query.q0_mut().single_mut().unwrap();
 
-    cam.transform_2_bevy(transform);
+    let (mut turret, _) = query.q0_mut().single_mut().unwrap();
+    turret.transform_2_bevy(transform);
 }
 
 fn update_barrels(
@@ -164,4 +174,14 @@ fn update_barrels(
     let (mut cam, _) = query.q0_mut().single_mut().unwrap();
 
     cam.transform_2_bevy(transform);
+}
+
+struct Rotates;
+
+fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rotates>>) {
+    for mut transform in query.iter_mut() {
+        *transform = Transform::from_rotation(bevy::math::Quat::from_rotation_y(
+            (4.0 * std::f32::consts::PI / 20.0) * time.delta_seconds(),
+        )) * *transform;
+    }
 }
