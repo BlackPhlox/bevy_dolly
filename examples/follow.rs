77dc740c24292050e7c928f7b502e16221834729
Mikkel Rasmussen <theluja@gmail.com>
2023-03-08 02:06:28 +0100

    Fixed dolly scaling issue, changed ergonomics with backwards compatibility

--- examples/follow.rs
+++ examples/follow.rs
@@ -10,10 +10,11 @@ fn main() {
     App::new()
         .insert_resource(Msaa::default())
         .add_plugins(DefaultPlugins)
-        .add_dolly_component(MainCamera)
         .add_startup_system(setup)
         .add_system(rotator_system)
         .add_system(update_camera)
+        //.add_dolly_component(MainCamera)
+        .add_system(Dolly::<MainCamera>::update_active)
         .run();
 }
 
--- src/system.rs
+++ src/system.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use crate::prelude::*;
 use bevy::prelude::*;
 
@@ -12,75 +14,151 @@ pub trait DollyComponent {
 
 impl DollyComponent for App {
     fn add_dolly_component<T: Component>(&mut self, _: T) -> &mut Self {
-        self.add_system(dolly_component_cam_change_detection::<T>.in_set(DollyComponentLabel))
+        self.add_system(Dolly::<T>::update_active.in_set(DollyComponentLabel))
     }
 
     fn add_rig_component<T: Component>(&mut self, _: T) -> &mut Self {
-        self.add_system(dolly_component_change_detection::<T>.in_set(DollyComponentLabel))
+        self.add_system(Dolly::<T>::update_all.in_set(DollyComponentLabel))
     }
 
     fn add_dolly_2d_component<T: Component>(&mut self, _: T) -> &mut Self {
-        self.add_system(dolly_2d_component_cam_change_detection::<T>.in_set(DollyComponentLabel))
+        self.add_system(Dolly::<T>::update_2d_active_continuous.in_set(DollyComponentLabel))
     }
 }
 
-#[allow(clippy::type_complexity)]
-pub fn dolly_component_cam_change_detection<T: Component>(
-    mut cameras: Query<(&mut Transform, &Camera), With<T>>,
-    time: Res<Time>,
-    mut query: Query<(Entity, &mut Rig), (Changed<Rig>, With<T>)>,
-) {
-    for (_entity, mut rig) in &mut query {
-        //let d = rig.drivers.iter().map(|f| format!("{:?}", f)).collect::<Vec<String>>().join(", ");
-        //info!("{:?} changed: {:?}", entity, d);
+pub struct Dolly<T: Component> {
+    _phantom: PhantomData<T>,
+}
 
-        let transform = rig.update(time.delta_seconds());
+impl<T> Dolly<T>
+where
+    T: Component,
+{
+    #[allow(clippy::type_complexity)]
+    pub fn update_active(
+        mut cameras: Query<(&mut Transform, &Camera), With<T>>,
+        time: Res<Time>,
+        mut query: Query<&mut Rig, (Changed<Rig>, With<T>)>,
+    ) {
+        for mut rig in &mut query {
+            //info!("{:?} changed: {:?}", entity, d);
 
-        cameras.for_each_mut(|(mut t, camera)| {
-            if camera.is_active {
+            let transform = rig.update(time.delta_seconds());
+
+            cameras.for_each_mut(|(mut t, camera)| {
+                if camera.is_active {
+                    *t = transform;
+                }
+            });
+        }
+    }
+
+    const SCALE_INCR_THRESHOLD: f32 = 0.0025;
+    const RANGE_SCALE_2D: f32 = 1. + Self::SCALE_INCR_THRESHOLD;
+
+    pub fn update_2d_active(
+        mut cameras: Query<(&mut Transform, &mut OrthographicProjection, &Camera), With<T>>,
+        time: Res<Time>,
+        mut query: Query<&mut Rig, (Changed<Rig>, With<T>)>,
+    ) {
+        for mut rig in &mut query {
+            let mut transform = rig.update(time.delta_seconds());
+            cameras.for_each_mut(|(mut t, mut orth, camera)| {
+                if camera.is_active {
+                    //Bind camera's Z axis to scale, if used for init state check to prevent scale of 0
+                    if !(transform.translation.z < Self::RANGE_SCALE_2D
+                        && transform.translation.z > -Self::RANGE_SCALE_2D)
+                    {
+                        orth.scale = (transform.translation.z + 1.) * Self::SCALE_INCR_THRESHOLD;
+                    }
+                    //Drop Z from camera's transform calculations
+                    let xy = transform.translation.truncate().extend(0 as f32);
+                    transform.translation = xy;
+                    *t = transform;
+                }
+            });
+        }
+    }
+
+    #[allow(clippy::type_complexity)]
+    pub fn update_all(
+        mut transforms: Query<&mut Transform, With<T>>,
+        time: Res<Time>,
+        mut query: Query<&mut Rig, (Changed<Rig>, With<T>)>,
+    ) {
+        for mut rig in &mut query {
+            //info!("{:?} changed: {:?}", entity, d);
+
+            let transform = rig.update(time.delta_seconds());
+
+            transforms.for_each_mut(|mut t| {
                 *t = transform;
-            }
-        });
+            });
+        }
     }
-}
 
-#[allow(clippy::type_complexity)]
-pub fn dolly_2d_component_cam_change_detection<T: Component>(
-    mut cameras: Query<(&mut Transform, &mut OrthographicProjection, &Camera), With<T>>,
-    time: Res<Time>,
-    mut query: Query<(Entity, &mut Rig), (Changed<Rig>, With<T>)>,
-) {
-    for (_entity, mut rig) in &mut query {
-        //let d = rig.drivers.iter().map(|f| format!("{:?}", f)).collect::<Vec<String>>().join(", ");
-        //info!("{:?} changed: {:?}", entity, d);
+    // Continuous versions
+    // Per default dolly only updates on input (change to rig)
+    // This reduces update calls.
+    // However, if camera movement is expected to last after input, ie. using large amount of motion smoothing < 0.25
+    // Use the systems below instead.
 
-        let mut transform = rig.update(time.delta_seconds());
+    pub fn update_active_continuous(
+        mut cameras: Query<(&mut Transform, &Camera), With<T>>,
+        time: Res<Time>,
+        mut query: Query<&mut Rig, With<T>>,
+    ) {
+        for mut rig in &mut query {
+            //info!("{:?} changed: {:?}", entity, d);
 
-        cameras.for_each_mut(|(mut t, mut orth, camera)| {
-            if camera.is_active {
-                orth.scale = transform.translation.z * 0.0025; //.clamp(0.0, 0.5);
-                let xy = transform.translation.truncate().extend(0 as f32);
-                transform.translation = xy;
+            let transform = rig.update(time.delta_seconds());
+
+            cameras.for_each_mut(|(mut t, camera)| {
+                if camera.is_active {
+                    *t = transform;
+                }
+            });
+        }
+    }
+
+    #[allow(clippy::type_complexity)]
+    pub fn update_2d_active_continuous(
+        mut cameras: Query<(&mut Transform, &mut OrthographicProjection, &Camera), With<T>>,
+        time: Res<Time>,
+        mut query: Query<&mut Rig, With<T>>,
+    ) {
+        for mut rig in &mut query {
+            let mut transform = rig.update(time.delta_seconds());
+            cameras.for_each_mut(|(mut t, mut orth, camera)| {
+                if camera.is_active {
+                    //Bind camera's Z axis to scale, if used for init state check to prevent scale of 0
+                    if !(transform.translation.z < Self::RANGE_SCALE_2D
+                        && transform.translation.z > -Self::RANGE_SCALE_2D)
+                    {
+                        orth.scale = (transform.translation.z + 1.) * Self::SCALE_INCR_THRESHOLD;
+                    }
+                    //Drop Z from camera's transform calculations
+                    let xy = transform.translation.truncate().extend(0 as f32);
+                    transform.translation = xy;
+                    *t = transform;
+                }
+            });
+        }
+    }
+
+    pub fn update_all_continuous(
+        mut transforms: Query<&mut Transform, With<T>>,
+        time: Res<Time>,
+        mut query: Query<&mut Rig, With<T>>,
+    ) {
+        for mut rig in &mut query {
+            //info!("{:?} changed: {:?}", entity, d);
+
+            let transform = rig.update(time.delta_seconds());
+
+            transforms.for_each_mut(|mut t| {
                 *t = transform;
-            }
-        });
-    }
-}
-
-#[allow(clippy::type_complexity)]
-pub fn dolly_component_change_detection<T: Component>(
-    mut transforms: Query<&mut Transform, With<T>>,
-    time: Res<Time>,
-    mut query: Query<(Entity, &mut Rig), (Changed<Rig>, With<T>)>,
-) {
-    for (_entity, mut rig) in &mut query {
-        //let d = rig.drivers.iter().map(|f| format!("{:?}", f)).collect::<Vec<String>>().join(", ");
-        //info!("{:?} changed: {:?}", entity, d);
-
-        let transform = rig.update(time.delta_seconds());
-
-        transforms.for_each_mut(|mut t| {
-            *t = transform;
-        });
+            });
+        }
     }
 }
