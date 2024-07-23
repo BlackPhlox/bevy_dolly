fd9744c5d4c50a1d818589b61c91ed238442fca2
Mikkel Rasmussen <theluja@gmail.com>
2024-07-23 20:56:58 +0200

    Ran crate prep script

---
+++ src/dolly/driver.rs
@@ -0,0 +1,31 @@
+use bevy_transform::prelude::Transform;
+
+//use crate::dolly::{handedness::Handedness, rig::RigUpdateParams, transform::Transform};
+use crate::dolly::rig::RigUpdateParams;
+
+pub trait RigDriverTraits: RigDriver + Sync + Send + std::any::Any + std::fmt::Debug {
+    /// Returns `self` as `&dyn Any`
+    fn as_any(&self) -> &dyn std::any::Any;
+
+    /// Returns `self` as `&mut dyn Any`
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any;
+}
+
+pub trait RigDriver: std::any::Any + std::fmt::Debug {
+    /// Calculates the transform of this driver component based on the parent
+    /// provided in `params`.
+    fn update(&mut self, params: RigUpdateParams) -> Transform;
+}
+
+impl<T> RigDriverTraits for T
+where
+    T: RigDriver + std::any::Any + Sync + Send + std::fmt::Debug,
+{
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self
+    }
+}
---
+++ src/dolly/transform.rs
@@ -0,0 +1,52 @@
+use core::fmt::Debug;
+//use glam::{Quat, Vec3};
+use bevy_math::{Quat, Vec3};
+use std::marker::PhantomData;
+
+use crate::dolly::handedness::Handedness;
+
+/// A thin wrapper over a `Vec3` and a `Quat`
+#[derive(Clone, Copy, Debug)]
+pub struct Transform<H: Handedness> {
+    pub position: Vec3,
+    pub rotation: Quat,
+    pub phantom: PhantomData<H>,
+}
+
+impl<H: Handedness> Transform<H> {
+    ///
+    pub fn from_position_rotation(position: Vec3, rotation: Quat) -> Self {
+        Self {
+            position,
+            rotation,
+            phantom: PhantomData,
+        }
+    }
+
+    ///
+    pub fn into_position_rotation(self) -> (Vec3, Quat) {
+        (self.position, self.rotation)
+    }
+
+    /// +X
+    pub fn right(&self) -> Vec3 {
+        self.rotation * Vec3::X
+    }
+
+    /// +Y
+    pub fn up(&self) -> Vec3 {
+        self.rotation * Vec3::Y
+    }
+
+    /// +/-Z
+    pub fn forward(&self) -> Vec3 {
+        self.rotation * H::FORWARD
+    }
+
+    ///
+    pub const IDENTITY: Transform<H> = Transform {
+        position: Vec3::ZERO,
+        rotation: Quat::IDENTITY,
+        phantom: PhantomData,
+    };
+}
